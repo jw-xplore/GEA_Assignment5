21730c491de5dd5b36265f945955cc1f641daa70
jw-xplore <jwernerltug@gmail.com>
2026-01-20 17:39:01 +0100

    Added: Objects allocation counting

--- projects/spacegame/code/main.cc
+++ projects/spacegame/code/main.cc
@@ -4,9 +4,61 @@
 //------------------------------------------------------------------------------
 #include "config.h"
 #include "spacegameapp.h"
+#include <iostream>
+#include <new>
+#include <map>
 
-int
-main(int argc, const char** argv)
+//-------------------------------------------------------------------------
+// Memory manager test
+//-------------------------------------------------------------------------
+
+uint objectsCount;
+uint allocatedMemoryForObjects;
+uint arraysCount;
+uint allocatedMemoryForArrays;
+
+void* operator new(std::size_t size)
+{
+	if (void* p = std::malloc(size))
+	{
+		objectsCount++;
+		allocatedMemoryForObjects += size;
+		return p;
+	}
+
+	throw std::bad_alloc{};
+}
+
+void operator delete(void* p) noexcept
+{
+	objectsCount--;
+	allocatedMemoryForObjects -= sizeof(p);
+	std::free(p);
+}
+
+void* operator new[](std::size_t size) throw(std::bad_alloc)
+{
+	if (void* p = std::malloc(size))
+	{
+		arraysCount++;
+		allocatedMemoryForArrays += size;
+		return p;
+	}
+
+	throw std::bad_alloc{};
+}
+void operator delete[](void* p) throw()
+{
+	arraysCount--;
+	allocatedMemoryForObjects -= sizeof(p);
+	std::free(p);
+}
+
+//-------------------------------------------------------------------------
+// Main
+//-------------------------------------------------------------------------
+
+int main(int argc, const char** argv)
 {
 	Game::SpaceGameApp app;
 	if (app.Open())
@@ -16,4 +68,10 @@ main(int argc, const char** argv)
 	}
 	app.Exit();
 	
+	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	//_CrtDumpMemoryLeaks();
+
+	std::cout << "----------------------------------------------------" << "\n";
+	std::cout << "Leaked objects: " << objectsCount << ", size: " << allocatedMemoryForObjects << "\n";
+	std::cout << "Leaked arrays: " << arraysCount << ", size: " << allocatedMemoryForArrays;
 }
