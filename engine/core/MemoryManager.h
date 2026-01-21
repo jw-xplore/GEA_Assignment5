834e03f6ec21a428944e5e66cb2cd26f41633d73
Jakub Werner-local <jakwer-4@student.ltu.se>
2026-01-21 15:19:22 +0100

    Changed: Cmake min version, Added: Memory manager with base for memory pool/arena

---
+++ engine/core/MemoryManager.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+
+//-------------------------------------------------------------------------
+// Base memory manager
+//-------------------------------------------------------------------------
+
+struct MemoryPool
+{
+	void* memory;
+	size_t size;
+	unsigned int lastPosition;
+
+	MemoryPool(size_t s)
+	{
+		size = s;
+		memory = new void[size];
+	}
+};
+
+namespace MemoryManager
+{
+	const size_t POOL_SIZE = 65536;
+
+	unsigned int objectsCount;
+	unsigned int allocatedMemoryForObjects;
+	unsigned int arraysCount;
+	unsigned int allocatedMemoryForArrays;
+
+	void ReportMemoryAllocation()
+	{
+		std::cout << "----------------------------------------------------" << "\n";
+		std::cout << "MEMORY MANAGER REPORT:\n";
+		std::cout << "Allocated objects: " << objectsCount << ", size: " << allocatedMemoryForObjects << "\n";
+		std::cout << "Allocated arrays: " << arraysCount << ", size: " << allocatedMemoryForArrays << "\n";
+		std::cout << "----------------------------------------------------" << "\n";
+	}
+
+	//-------------------------------------------------------------------------
+	// Memory pool
+	//-------------------------------------------------------------------------
+
+	MemoryPool* AllocateMemoryPool(size_t size = POOL_SIZE)
+	{
+		MemoryPool* pool = new MemoryPool(size);
+		return pool;
+	}
+
+	void FreeMemoryPool(MemoryPool* pool)
+	{
+		delete pool;
+	}
+
+	/*
+	void* PoolPush(MemoryPool* pool, size_t size)
+	{
+
+	}
+	*/
+}
+
+//-------------------------------------------------------------------------
+// Tracking memory override
+//-------------------------------------------------------------------------
+
+void* operator new(std::size_t size)
+{
+	if (void* p = std::malloc(size))
+	{
+		MemoryManager::objectsCount++;
+		MemoryManager::allocatedMemoryForObjects += size;
+		return p;
+	}
+
+	throw std::bad_alloc{};
+}
+
+void operator delete(void* p) noexcept
+{
+	MemoryManager::objectsCount--;
+	MemoryManager::allocatedMemoryForObjects -= sizeof(p);
+	std::free(p);
+}
+
+void* operator new[](std::size_t size) throw(std::bad_alloc)
+{
+	if (void* p = std::malloc(size))
+	{
+		MemoryManager::arraysCount++;
+		MemoryManager::allocatedMemoryForArrays += size;
+		return p;
+	}
+
+	throw std::bad_alloc{};
+}
+void operator delete[](void* p) throw()
+{
+	MemoryManager::arraysCount--;
+	MemoryManager::allocatedMemoryForObjects -= sizeof(p);
+	std::free(p);
+}
--- projects/spacegame/code/main.cc
+++ projects/spacegame/code/main.cc
@@ -5,54 +5,7 @@
 #include "config.h"
 #include "spacegameapp.h"
 #include <iostream>
-#include <new>
-#include <map>
-
-//-------------------------------------------------------------------------
-// Memory manager test
-//-------------------------------------------------------------------------
-
-uint objectsCount;
-uint allocatedMemoryForObjects;
-uint arraysCount;
-uint allocatedMemoryForArrays;
-
-void* operator new(std::size_t size)
-{
-	if (void* p = std::malloc(size))
-	{
-		objectsCount++;
-		allocatedMemoryForObjects += size;
-		return p;
-	}
-
-	throw std::bad_alloc{};
-}
-
-void operator delete(void* p) noexcept
-{
-	objectsCount--;
-	allocatedMemoryForObjects -= sizeof(p);
-	std::free(p);
-}
-
-void* operator new[](std::size_t size) throw(std::bad_alloc)
-{
-	if (void* p = std::malloc(size))
-	{
-		arraysCount++;
-		allocatedMemoryForArrays += size;
-		return p;
-	}
-
-	throw std::bad_alloc{};
-}
-void operator delete[](void* p) throw()
-{
-	arraysCount--;
-	allocatedMemoryForObjects -= sizeof(p);
-	std::free(p);
-}
+#include "../engine/core/MemoryManager.h"
 
 //-------------------------------------------------------------------------
 // Main
@@ -71,7 +24,5 @@ int main(int argc, const char** argv)
 	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 	//_CrtDumpMemoryLeaks();
 
-	std::cout << "----------------------------------------------------" << "\n";
-	std::cout << "Leaked objects: " << objectsCount << ", size: " << allocatedMemoryForObjects << "\n";
-	std::cout << "Leaked arrays: " << arraysCount << ", size: " << allocatedMemoryForArrays;
+	MemoryManager::ReportMemoryAllocation();
 }
